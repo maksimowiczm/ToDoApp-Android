a5859dfb51e95df0951b56528b96e60fd217e96d
matmak13 <maksimowiczmateusz13@gmail.com>
2022-12-28 13:35:52 +0100

    Zachowanie stanu wyszukiwania po obróceniu ekranu

--- app/src/main/java/com/example/myapplication/TasksListActivity.kt
+++ app/src/main/java/com/example/myapplication/TasksListActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
@@ -23,7 +24,10 @@ class TasksListActivity : AppCompatActivity() {
     companion object {
         const val TASK: String = "task"
         const val NEW_TASK: String = "newTask"
+        const val SEARCH_QUERY: String = "searchQuery"
     }
+
+    private var searchQuery: String? = null
 
     private lateinit var recyclerView: RecyclerView
     private val adapter: TaskAdapter?
@@ -31,7 +35,15 @@ class TasksListActivity : AppCompatActivity() {
 
     private lateinit var addButton: FloatingActionButton
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d("rot", "onSaveInstanceState")
+        outState.putString(SEARCH_QUERY, searchQuery)
+
+        super.onSaveInstanceState(outState)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d("rot", "onCreateOptionsMenu")
         menuInflater.inflate(R.menu.main_menu, menu)
 
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
@@ -40,20 +52,32 @@ class TasksListActivity : AppCompatActivity() {
             setSearchableInfo(searchManager.getSearchableInfo(componentName))
         }
 
+        if (searchQuery != null) {
+            searchView.isIconified = true
+            searchView.onActionViewExpanded()
+            searchView.setQuery(searchQuery, false)
+            searchView.isFocusable = true
+        }
+
+        val repo = TaskRepository(TaskDatabase.getInstance(application).taskDao())
+        val tasks = if (searchQuery != null) repo.findTasks(searchQuery!!) else repo.getAll
+        tasks.observe(this@TasksListActivity) { tasks ->
+            adapter!!.submitList(tasks)
+        }
+
         searchView.setOnQueryTextListener(object : OnQueryTextListener {
-            val repo = TaskRepository(TaskDatabase.getInstance(application).taskDao())
 
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
-                if (newText != null) {
-                    val tasks = repo.findTasks(newText!!)
-                    tasks.observe(this@TasksListActivity) { tasks ->
-                        adapter!!.submitList(tasks)
-                    }
+                searchQuery = newText
+                val tasks = repo.findTasks(searchQuery!!)
+                tasks.observe(this@TasksListActivity) { tasks ->
+                    adapter!!.submitList(tasks)
                 }
+
                 return true
             }
         })
@@ -64,6 +88,9 @@ class TasksListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks_list)
+
+        if (savedInstanceState != null)
+            searchQuery = savedInstanceState.getString(SEARCH_QUERY)
 
         recyclerView = findViewById(R.id.recycler_view)
         addButton = findViewById(R.id.add_task)
